91d5d68ecb8be85ab70b89fcd9078f3bdd56e3e3
Caid Buchman <caibuch2010@gmail.com>
2017-01-31 20:47:37 -0600

    Rough working protype

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ submit_form = """
     <form method="post">
         <label>
             Username
-            <input type="text" name="username"/>
+            <input type="text" name="username" value="%(username_escaped)s"/>
         </label> <span style="color: red">%(usererror)s</span> <br>
         <label>
             Password
@@ -48,31 +48,28 @@ submit_form = """
         </label> <span style="color: red">%(passworderror)s</span> <br>
         <label>
             Verify password
-            <input type="text" name="verify"/>
+            <input type="password" name="verify"/>
         </label> <span style="color: red">%(verifyerror)s</span> <br>
         <label>
             Email(optional)
-            <input type="text" name="email"/>
-        </label> <br>
+            <input type="text" name="email" value="%(email_escaped)s"/>
+        </label> <span style="color: red">%(emailerror)s<br>
             <input type="submit" value="Submit"/>
         </form>
             """
 
 
 class Index(webapp2.RequestHandler):
-    def writeForm(self, ue="", pe="", ve=""):
-        edict = {"usererror":ue, "passworderror":pe, "verifyerror":ve}
+    def writeForm(self, ue="", pe="", ve="", ee="",uescaped="",escaped=""):
+        edict = {"usererror":ue, "passworderror":pe, "verifyerror":ve, "emailerror":ee, "username_escaped":uescaped, "email_escaped":escaped}
         self.response.write(page_header + (submit_form % edict) + page_footer)
 
     def get(self):
         self.writeForm()
-        #self.response.write(page_header + submit_form + page_footer)
-
-
 
     def post(self):
         username = self.request.get("username")
-        #username_escaped = cgi.escape(username)
+        username_escaped = cgi.escape(username)
         password = self.request.get("password")
         password_escaped = cgi.escape(password)
         verify = self.request.get("verify")
@@ -89,34 +86,42 @@ class Index(webapp2.RequestHandler):
 
         USER_pass = re.compile(r"^.{3,20}$")
         def valid_password(passwordInput):
-            if USER_RE.match(passwordInput):
+            if USER_pass.match(passwordInput):
                 return True
             else:
                 return False
 
-        self.response.write(valid_password(password))
+        USER_email = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+        def invalid_email(emailInput):
+            if USER_email.match(emailInput):
+                return True
+            else:
+                return False
 
-        #ue = ""
-        #pe=""
-        #if username == "":
-            #self.response.write(page_header + page_footer)
-            #ue ="no username"
-        #if
-        #self.writeForm(ue ,pe)
+        ue = ""
+        pe= ""
+        ve= ""
+        ee= ""
+        uescaped = username_escaped
+        escaped = email_escaped
 
+        if username == "" or valid_username(username) == False:
+            ue += "That isn't a valid username"
 
+        if password == "" or valid_password(password) == False:
+            pe += "That isn't a valid password"
 
-        #if valid_username(username_escaped) == False:
-            #self.response.write(page_header + submit_form + username_error + page_footer)
-        #else:
-            #self.response.write("<h1>Success</h1>")
+        if password != verify:
+            ve += "Your passwords don't match"
 
-        #if valid_username(username_escaped) == True:
+        if invalid_email(email) == False:
+            ee += "That isn't a valid email"
+
+        if invalid_email(email) == True and valid_username(username) == True and valid_password(password) == True and password == verify:
             #self.redirect("/welcome" + confirmation)
+            self.redirect("/welcome?username=" + username_escaped)
 
-        #self.redirect("/welcome?username=" + username_escaped)
-
-        #self.response.write(page_header + submit_form + "<p>" + "hello" + "</p>" + page_footer)
+        self.writeForm(ue,pe,ve,ee,uescaped,escaped)
 
 
 class Welcome(webapp2.RequestHandler):
